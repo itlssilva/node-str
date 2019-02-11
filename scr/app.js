e62cabef749d46f7bfac247aedd758b8bc4a8001
itlssilva <italo.santosilva@gmail.com>
2019-02-11 21:54:12 -0200

    Criação do repositório

---
+++ scr/app.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+const router = express.Router();
+
+// Conecta ao banco
+mongoose.connect('mongodb://admin:ig0r110312@ds129045.mlab.com:29045/ndstr');
+
+// Carrega os Models
+const Product = require('./models/product');
+
+// Carrega as Rotas
+const indexRoute = require('./routes/index-route');
+const productRoute = require('./routes/product-route');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', indexRoute);
+app.use('/products', productRoute);
+
+
+module.exports = app;
