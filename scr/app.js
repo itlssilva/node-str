35865ec9da3440a10849a8f837300352f51fca1a
itlssilva <italo.santosilva@gmail.com>
2019-03-04 18:33:17 -0300

    Ajuste da Controller product - criação das classes de Order e Customer

--- scr/app.js
+++ scr/app.js
@@ -12,15 +12,19 @@ mongoose.connect('mongodb://admin:ig0r110312@ds129045.mlab.com:29045/ndstr');
 
 // Carrega os Models
 const Product = require('./models/product');
+const Customer = require('./models/customer');
+const Order = require('./models/order');
 
 // Carrega as Rotas
 const indexRoute = require('./routes/index-route');
 const productRoute = require('./routes/product-route');
+const customerRoute = require('./routes/customer-route');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/', indexRoute);
 app.use('/products', productRoute);
+app.use('/customers', customerRoute);
 
 module.exports = app;
